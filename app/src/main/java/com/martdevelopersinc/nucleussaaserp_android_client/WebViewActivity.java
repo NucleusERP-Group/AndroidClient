400eb05157203c1790c4d60b5a866dc70a46203c
MartMbithi <martdevelopers254@gmail.com>
2021-05-20 14:24:39 +0300

    Added Fullsreen Activity & WebView

--- app/src/main/java/com/martdevelopersinc/nucleussaaserp_android_client/WebViewActivity.java
+++ app/src/main/java/com/martdevelopersinc/nucleussaaserp_android_client/WebViewActivity.java
@@ -145,10 +145,7 @@ public class WebViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web_view);
 
         mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
 
-        // Set up the user interaction to manually show or hide the system UI.
         mContentView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
